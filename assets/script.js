9c25a879c2765d43f60842b9ed9dede10f6396a7
Angelo <angeloantosilva@gmail.com>
2022-10-17 00:12:15 +0100

    add conditionals to javascript, if and for

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+let topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+/*
+let shapes = ["triangle", "square", "pentagon", "circle"];
+
+for(var x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);
+}
+*/
