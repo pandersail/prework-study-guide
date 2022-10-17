0cbee6643e0c8537dd9a4f37e5af134b798064e9
Angelo <angeloantosilva@gmail.com>
2022-10-17 13:57:26 +0100

    add a function to JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,21 @@
-let topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for(let x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+function listTopics() {
+ for (var x = 0; x < topics.length; x++) {
+   console.log(topics[x]);
+ }
 }
 
-/*
-let shapes = ["triangle", "square", "pentagon", "circle"];
-
-for(var x = 0; x < shapes.length; x++) {
- console.log(shapes[x]);
+function selectTopic() {
+ if (topic === 'HTML') {
+   console.log("Let's study HTML!");
+ } else if (topic === 'CSS') {
+   console.log("Let's study CSS!");
+ } else if (topic === 'Git') {
+   console.log("Let's study Git!");
+ } else if (topic === 'JavaScript') {
+   console.log("Let's study JavaScript!");
+ } else {
+   console.log('Please try again!');
+ }
 }
-*/
